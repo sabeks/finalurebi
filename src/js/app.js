bff501bb7edd9d8c7449eb62b9ff96e447561d26
saba aptsiauri <llamerchik@gmail.com>
2023-08-31 17:33:49 +0400

    added comments

--- src/js/app.js
+++ src/js/app.js
@@ -79,3 +79,27 @@ serviceContainer.addEventListener("mouseout", function (event) {
     target.querySelector(".service-icon").style.display = "inline-block";
   }
 });
+let commentsSlides = document.querySelectorAll(".comments-slide");
+let commentsButton1 = document.getElementById("comments-button-1");
+let commentsButton2 = document.getElementById("comments-button-2");
+let commentsButton3 = document.getElementById("comments-button-3");
+
+commentsSlides[0].classList.add("active");
+
+commentsButton1.addEventListener("click", function () {
+  commentsSlides[0].classList.add("active");
+  commentsSlides[1].classList.remove("active");
+  commentsSlides[2].classList.remove("active");
+});
+
+commentsButton2.addEventListener("click", function () {
+  commentsSlides[0].classList.remove("active");
+  commentsSlides[1].classList.add("active");
+  commentsSlides[2].classList.remove("active");
+});
+
+commentsButton3.addEventListener("click", function () {
+  commentsSlides[0].classList.remove("active");
+  commentsSlides[1].classList.remove("active");
+  commentsSlides[2].classList.add("active");
+});
